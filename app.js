5b02fd04aab4577fdc8dd07c486ad0c4376c8baf
ppijn <pepijnjong2000@gmail.com>
2022-03-22 12:59:49 +0100

    made partials and index.ejs with text so far

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+const http = require('http');
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + "/public"));
+// Set ejs in als template engine
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Stel een static map in
+app.use(express.static("/public"));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.get("/", renderPage);
+
+function renderPage(req, res) {
+  res.render('index', {
+    pagetitle: 'Dit is een express pagina'
+  });
+}
