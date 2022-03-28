f0165b35f765b7e2fc024eb17e078115aad1bbb0
ppijn <pepijnjong2000@gmail.com>
2022-03-28 15:32:41 +0200

    veel veranderingen in alles

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const http = require('http');
 const express = require("express");
+const { read } = require('fs');
 const app = express();
 const port = 3000;
+const fetch = (...args) => import("node-fetch").then(({ default: fetch}) => fetch(...args))
 
 // Set ejs in als template engine
 app.set("view engine", "ejs");
-app.set("views", "views");
+app.set("views", "./views");
 
 // Stel een static map in
 app.use(express.static("public"));
@@ -15,10 +17,60 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-app.get("/", renderPage);
 
-function renderPage(req, res) {
-  res.render('index', {
-    pagetitle: 'Dit is een express pagina'
-  });
-}
+app.get("/", async (request, response) => {
+  const rijksAPI =
+  "https://www.rijksmuseum.nl/api/nl/collection?key=8Rynz75W&p=0-n&ps=10&imgonly=true"
+  const options = {
+    method: 'GET'
+  }
+
+  const json = await fetch(rijksAPI, options)
+  .then(res => res.json())
+  .catch(e => {
+    console.error({
+      'message': 'oh no',
+      error: e,
+    })
+  })
+  // console.log(json.artObjects)
+  response.render("index", {
+    data: json.artObjects
+  })
+});
+
+app.get("/search", async (request, response) => {
+  const rijksAPI =
+  `https://www.rijksmuseum.nl/api/nl/collection?key=8Rynz75W&q=${request.query.q}&p=0-n&ps=10&imgonly=true`
+  const options = {
+    method: 'GET'
+  }
+  console.log(request.query.q)
+  
+  const json = await fetch(rijksAPI, options)
+  .then(res => res.json())
+  .catch(e => {
+    console.error({
+      'message': 'oh no',
+      error: e,
+    })
+  })
+  console.log(json.artObjects)
+  response.render("index", {
+    data: json.artObjects
+  })
+});
+
+// app.get("/home", async (request, response) => {
+//   console.log(response)
+//   res.render('art-list', {
+//     data: response.artObjects
+//   })
+// })
+
+
+// function renderPage(req, res) {
+//   res.render('index', {
+//     pagetitle: 'Dit is een express pagina'
+//   });
+// }
