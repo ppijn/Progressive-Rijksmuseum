c62ea89132e4d8cd019a5c5976216a579d85a326
ppijn <pepijnjong2000@gmail.com>
2022-04-01 14:10:55 +0200

    sw beginning of cache

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,30 @@
-// Install service worker
+const staticCacheName = "site-static";
+const assets = [
+  '/',
+  '/views/index.ejs',
+  '/views/home.ejs',
+  '/views/partials/foot.ejs',
+  '/views/partials/head.ejs',
+  '/public/css/style.css',
+  '/public/img/rijksmuseum.png',
+  '/app.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+];
 
 self.addEventListener('install', evt => {
-console.log('service worker has been installed');
+  // console.log('service worker has been installed');
+  evt.waitUntil( 
+    caches.open(staticCacheName).then(cache => {
+     console.log('caching shell assets');
+     cache.addAll(assets);
+    })
+  );
 });
 
 // activate event
 
 self.addEventListener('activate', evt => {
-  console.log('service worker has been activated');
+  // console.log('service worker has been activated');
 });
 
 // fetch
@@ -15,3 +32,4 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
   // console.log('fetch event', evt);
 });
+
